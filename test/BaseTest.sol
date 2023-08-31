6b58109cf14ac1ce8617156b2d9b65b35b46ffec
Matt Solomon <matt@mattsolomon.dev>
2023-08-31 16:42:28 -0700

    test: add example spec and test

--- test/BaseTest.sol
+++ test/BaseTest.sol
@@ -4,9 +4,20 @@ pragma solidity 0.8.21;
 import {Test} from "forge-std/Test.sol";
 import {CreateX} from "../src/CreateX.sol";
 
+// Harness contract that exposes internal functions for testing.
+contract CreateXHarness is CreateX {
+    function exposed_efficientHash(bytes32 a, bytes32 b) external pure returns (bytes32 hash) {
+        return _efficientHash(a, b);
+    }
+}
+
+// Base test contract that deploys CreateX and CreateXHarness and defines events and helper methods.
 contract BaseTest is Test {
     CreateX internal createX;
     address internal createXAddr;
+
+    CreateXHarness internal createXHarness;
+    address internal createXHarnessAddr;
 
     address internal constant zeroAddress = address(0);
 
@@ -16,5 +27,8 @@ contract BaseTest is Test {
     function setUp() public {
         createX = new CreateX();
         createXAddr = address(createX);
+
+        createXHarness = new CreateXHarness();
+        createXHarnessAddr = address(createXHarness);
     }
 }
---
+++ test/CreateX._efficientHash.t.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.8.21;
+
+import {BaseTest} from "./BaseTest.sol";
+
+contract CreateX_EfficientHash_Test is BaseTest {
+    function test_Requirements(bytes32 a, bytes32 b) external {
+        // It should match the output of a high-level hash.
+        // It should not revert.
+        bytes32 expected = keccak256(abi.encodePacked(a, b));
+        bytes32 actual = createXHarness.exposed_efficientHash(a, b);
+        assertEq(actual, expected, "hash mismatch");
+    }
+}
