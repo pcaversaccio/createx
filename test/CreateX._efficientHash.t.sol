ee94a28d0e72dcb6029460fe862874745966113b
Matt Solomon <matt@mattsolomon.dev>
2023-09-05 14:27:47 -0700

    chore: add license comment

--- test/CreateX._efficientHash.t.sol
+++ test/CreateX._efficientHash.t.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: WTFPL
 pragma solidity 0.8.21;
 
 import {BaseTest} from "./BaseTest.sol";
@@ -7,7 +8,7 @@ contract CreateX_EfficientHash_Test is BaseTest {
         // It should match the output of a high-level hash.
         // It should not revert.
         bytes32 expected = keccak256(abi.encodePacked(a, b));
-        bytes32 actual = createXHarness.exposed_efficientHash(a, b);
-        assertEq(actual, expected, "hash mismatch");
+        bytes32 actual = createXHarness.exposed_efficientHash({a: a, b: b});
+        assertEq({a: actual, b: expected, err: "hash mismatch"});
     }
 }
