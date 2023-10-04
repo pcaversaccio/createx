c3db17d83ea0f8d2d9fdf41610da0dddc5f9e8ea
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-10-04 12:14:19 +0200

    ♻️ Simplify `deployCreate` tests
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/public/CreateX.deployCreate.t.sol
+++ test/public/CreateX.deployCreate.t.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.21;
 
 import {BaseTest} from "../utils/BaseTest.sol";
-import {ERC20Mock} from "../mocks/ERC20Mock.sol";
 import {ERC20MockPayable} from "../mocks/ERC20MockPayable.sol";
 import {CreateX} from "../../src/CreateX.sol";
 
@@ -19,9 +18,6 @@ contract CreateX_DeployCreate_External_Test is BaseTest {
     bytes internal args;
 
     bytes internal cachedInitCode;
-    bytes internal cachedInitCodePayable;
-
-    uint256 internal msgValue;
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                           EVENTS                           */
@@ -66,136 +62,30 @@ contract CreateX_DeployCreate_External_Test is BaseTest {
         arg3 = makeAddr("initialAccount");
         arg4 = 100;
         args = abi.encode(arg1, arg2, arg3, arg4);
-        cachedInitCode = abi.encodePacked(type(ERC20Mock).creationCode, args);
-        cachedInitCodePayable = abi.encodePacked(type(ERC20MockPayable).creationCode, args);
+        cachedInitCode = abi.encodePacked(type(ERC20MockPayable).creationCode, args);
     }
 
     modifier whenTheInitCodeCreatesAValidRuntimeBytecode() {
-        if (cachedInitCode.length == 0 || cachedInitCodePayable.length == 0) {
+        if (cachedInitCode.length == 0) {
             revert ZeroByteInitCode(SELF);
         }
         _;
     }
 
     modifier whenTheCreatedRuntimeBytecodeHasANonZeroLength() {
-        if (cachedInitCode.length == 0 || cachedInitCodePayable.length == 0) {
+        if (cachedInitCode.length == 0) {
             revert ZeroByteInitCode(SELF);
         }
         _;
     }
 
-    modifier whenTheInitCodeHasANonpayableConstructor() {
-        _;
-    }
-
-    modifier whenTheCallvalueIsZero() {
-        msgValue = 0;
-        _;
-    }
-
-    function testFuzz_WhenTheCallvalueIsZeroAndHasANonpayableConstructor(
-        uint64 nonce
-    )
-        external
-        whenTheInitCodeCreatesAValidRuntimeBytecode
-        whenTheCreatedRuntimeBytecodeHasANonZeroLength
-        whenTheInitCodeHasANonpayableConstructor
-        whenTheCallvalueIsZero
-    {
-        vm.assume(nonce != 0 && nonce < type(uint64).max);
-        vm.setNonce(createXAddr, nonce);
-        // We calculate the address beforehand where the contract is to be deployed.
-        address computedAddress = createX.computeCreateAddress(createXAddr, nonce);
-
-        // We also check for the ERC-20 standard `Transfer` event.
-        vm.expectEmit(true, true, true, true, computedAddress);
-        emit Transfer(zeroAddress, arg3, arg4);
-        // It returns a contract address with a non-zero bytecode length and a zero ether balance.
-        // It emits the event `ContractCreation` with the contract address as indexed argument.
-        vm.expectEmit(true, true, true, true, createXAddr);
-        emit ContractCreation(computedAddress);
-        address newContract = createX.deployCreate{value: msgValue}(cachedInitCode);
-
-        assertEq(newContract, computedAddress);
-        assertNotEq(newContract, zeroAddress);
-        assertNotEq(newContract.code.length, 0);
-        assertEq(newContract.balance, 0);
-        assertEq(ERC20Mock(computedAddress).name(), arg1);
-        assertEq(ERC20Mock(computedAddress).symbol(), arg2);
-        assertEq(ERC20Mock(computedAddress).balanceOf(arg3), arg4);
-    }
-
-    modifier whenTheCallvalueIsNonZero(uint256 value) {
-        msgValue = bound(value, 1, type(uint64).max);
-        _;
-    }
-
-    function testFuzz_WhenTheCallvalueIsNonZeroAndHasANonpayableConstructor(
+    function test_WhenTheCreatedRuntimeBytecodeHasANonZeroLength(
         uint64 nonce,
-        uint256 value
-    )
-        external
-        whenTheInitCodeCreatesAValidRuntimeBytecode
-        whenTheCreatedRuntimeBytecodeHasANonZeroLength
-        whenTheInitCodeHasANonpayableConstructor
-        whenTheCallvalueIsNonZero(value)
-    {
+        uint256 msgValue
+    ) external whenTheInitCodeCreatesAValidRuntimeBytecode whenTheCreatedRuntimeBytecodeHasANonZeroLength {
         vm.assume(nonce != 0 && nonce < type(uint64).max);
         vm.setNonce(createXAddr, nonce);
-        // It should revert.
-        bytes memory expectedErr = abi.encodeWithSelector(CreateX.FailedContractCreation.selector, createXAddr);
-        vm.expectRevert(expectedErr);
-        createX.deployCreate{value: msgValue}(cachedInitCode);
-    }
-
-    modifier whenTheInitCodeHasAPayableConstructor() {
-        _;
-    }
-
-    function testFuzz_WhenTheCallvalueIsZeroAndHasAPayableConstructor(
-        uint64 nonce
-    )
-        external
-        whenTheInitCodeCreatesAValidRuntimeBytecode
-        whenTheCreatedRuntimeBytecodeHasANonZeroLength
-        whenTheInitCodeHasAPayableConstructor
-        whenTheCallvalueIsZero
-    {
-        vm.assume(nonce != 0 && nonce < type(uint64).max);
-        vm.setNonce(createXAddr, nonce);
-        // We calculate the address beforehand where the contract is to be deployed.
-        address computedAddress = createX.computeCreateAddress(createXAddr, nonce);
-
-        // We also check for the ERC-20 standard `Transfer` event.
-        vm.expectEmit(true, true, true, true, computedAddress);
-        emit Transfer(zeroAddress, arg3, arg4);
-        // It returns a contract address with a non-zero bytecode length and a zero ether balance.
-        // It emits the event `ContractCreation` with the contract address as indexed argument.
-        vm.expectEmit(true, true, true, true, createXAddr);
-        emit ContractCreation(computedAddress);
-        address newContract = createX.deployCreate{value: msgValue}(cachedInitCodePayable);
-
-        assertEq(newContract, computedAddress);
-        assertNotEq(newContract, zeroAddress);
-        assertNotEq(newContract.code.length, 0);
-        assertEq(newContract.balance, 0);
-        assertEq(ERC20Mock(computedAddress).name(), arg1);
-        assertEq(ERC20Mock(computedAddress).symbol(), arg2);
-        assertEq(ERC20Mock(computedAddress).balanceOf(arg3), arg4);
-    }
-
-    function testFuzz_WhenTheCallvalueIsNonZeroAndHasAPayableConstructor(
-        uint64 nonce,
-        uint256 value
-    )
-        external
-        whenTheInitCodeCreatesAValidRuntimeBytecode
-        whenTheCreatedRuntimeBytecodeHasANonZeroLength
-        whenTheInitCodeHasAPayableConstructor
-        whenTheCallvalueIsNonZero(value)
-    {
-        vm.assume(nonce != 0 && nonce < type(uint64).max);
-        vm.setNonce(createXAddr, nonce);
+        msgValue = bound(msgValue, 0, type(uint64).max);
         // We calculate the address beforehand where the contract is to be deployed.
         address computedAddress = createX.computeCreateAddress(createXAddr, nonce);
 
@@ -203,18 +93,18 @@ contract CreateX_DeployCreate_External_Test is BaseTest {
         vm.expectEmit(true, true, true, true, computedAddress);
         emit Transfer(zeroAddress, arg3, arg4);
         // It returns a contract address with a non-zero bytecode length and a potential non-zero ether balance.
-        // It emits the event `ContractCreation` with the contract address as indexed argument.
+        // It emits the event ContractCreation with the contract address as indexed argument.
         vm.expectEmit(true, true, true, true, createXAddr);
         emit ContractCreation(computedAddress);
-        address newContract = createX.deployCreate{value: msgValue}(cachedInitCodePayable);
+        address newContract = createX.deployCreate{value: msgValue}(cachedInitCode);
 
         assertEq(newContract, computedAddress);
         assertNotEq(newContract, zeroAddress);
         assertNotEq(newContract.code.length, 0);
         assertEq(newContract.balance, msgValue);
-        assertEq(ERC20Mock(computedAddress).name(), arg1);
-        assertEq(ERC20Mock(computedAddress).symbol(), arg2);
-        assertEq(ERC20Mock(computedAddress).balanceOf(arg3), arg4);
+        assertEq(ERC20MockPayable(computedAddress).name(), arg1);
+        assertEq(ERC20MockPayable(computedAddress).symbol(), arg2);
+        assertEq(ERC20MockPayable(computedAddress).balanceOf(arg3), arg4);
     }
 
     modifier whenTheCreatedRuntimeBytecodeHasAZeroLength() {
@@ -222,10 +112,12 @@ contract CreateX_DeployCreate_External_Test is BaseTest {
     }
 
     function testFuzz_WhenTheCreatedRuntimeBytecodeHasAZeroLength(
-        uint64 nonce
+        uint64 nonce,
+        uint256 msgValue
     ) external whenTheInitCodeCreatesAValidRuntimeBytecode whenTheCreatedRuntimeBytecodeHasAZeroLength {
         vm.assume(nonce != 0 && nonce < type(uint64).max);
         vm.setNonce(createXAddr, nonce);
+        msgValue = bound(msgValue, 0, type(uint64).max);
         // It should revert.
         bytes memory expectedErr = abi.encodeWithSelector(CreateX.FailedContractCreation.selector, createXAddr);
         vm.expectRevert(expectedErr);
@@ -236,10 +128,13 @@ contract CreateX_DeployCreate_External_Test is BaseTest {
         _;
     }
 
-    function testFuzz_WhenTheContractCreationFails(uint64 nonce) external whenTheContractCreationFails {
+    function testFuzz_WhenTheContractCreationFails(
+        uint64 nonce,
+        uint256 msgValue
+    ) external whenTheContractCreationFails {
         vm.assume(nonce != 0 && nonce < type(uint64).max);
         vm.setNonce(createXAddr, nonce);
-
+        msgValue = bound(msgValue, 0, type(uint64).max);
         // The following contract creation code contains the invalid opcode `PUSH0` (`0x5F`) and `CREATE` must therefore
         // return the zero address (technically zero bytes `0x`), as the deployment fails. This test also ensures that if
         // we ever accidentally change the EVM version in Foundry and Hardhat, we will always have a corresponding failed test.
