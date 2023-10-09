d90988f67c980b6a01ac79708be15e2595ef6aca
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-10-09 17:55:29 +0200

    🔧 Add `computeCreate2Address` tests
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

---
+++ test/public/CREATE2/CreateX.computeCreate2Address_2Args.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import {BaseTest} from "../../utils/BaseTest.sol";
+import {CreateX} from "../../../src/CreateX.sol";
+
+contract CreateX_ComputeCreate2Address_2Args_Public_Test is BaseTest {
+    function testFuzz_ReturnsThe20ByteAddressWhereAContractWillBeStoredAndShouldNeverRevert(bytes32 salt) external {
+        vm.startPrank(createXAddr);
+        address createAddressComputedOnChain = address(new CreateX{salt: salt}());
+        vm.stopPrank();
+        // It returns the 20-byte address where a contract will be stored.
+        // It should never revert.
+        assertEq(
+            createX.computeCreate2Address(salt, keccak256(type(CreateX).creationCode)),
+            createAddressComputedOnChain
+        );
+    }
+}
