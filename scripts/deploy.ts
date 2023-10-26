3b7e105980337213e7076f7f19a75612f3c7585e
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-10-26 14:24:55 +0200

    ♻️  Add delay for proper verification
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,14 +1,21 @@
 import hre, { ethers } from "hardhat";
 
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function main() {
   const contract = await ethers.deployContract("CreateX");
 
   await contract.waitForDeployment();
+  const contractAddress = await contract.getAddress();
 
-  console.log("CreateX deployed to:", await contract.getAddress());
+  console.log("CreateX deployed to:", contractAddress);
+
+  await delay(30000); // Wait for 30 seconds before verifying the contract
 
   await hre.run("verify:verify", {
-    address: contract.getAddress(),
+    address: contractAddress,
   });
 }
 
