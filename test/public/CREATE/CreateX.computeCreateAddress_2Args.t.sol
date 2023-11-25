112463ee260afd920e3f911d782308775fef4049
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-11-25 08:50:59 +0100

    ♻️  Refactor
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/public/CREATE/CreateX.computeCreateAddress_2Args.t.sol
+++ test/public/CREATE/CreateX.computeCreateAddress_2Args.t.sol
@@ -16,11 +16,10 @@ contract CreateX_ComputeCreateAddress_2Args_Public_Test is BaseTest {
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     modifier whenTheNonceValueDoesNotExceed18446744073709551614(address deployer, uint64 nonce) {
-        vm.assume(nonce < type(uint64).max);
+        vm.assume(nonce < type(uint64).max && nonce >= vm.getNonce(deployer));
         if (deployer.code.length != 0) {
             vm.assume(nonce != 0);
         }
-        vm.assume(nonce >= vm.getNonce(deployer));
         vm.setNonce(deployer, nonce);
         _;
     }
