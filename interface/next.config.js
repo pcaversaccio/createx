3dfa2ddf1b05be27335edb8d83390a23bb615b97
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-12-16 12:20:05 +0100

    👷 Add `deploy.yml` script
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

---
+++ interface/next.config.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+    /**
+     * Enable static exports for the App Router.
+     *
+     * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+     */
+    output: "export",
+  
+    /**
+     * Set base path. This is usually the slug of your repository.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
+    basePath: "/createx",
+  
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+     */
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  module.exports = nextConfig;
