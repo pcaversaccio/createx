8782099372204eb181e85b860a52a4a0ef97c5f1
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-12-17 12:50:23 +0100

    ♻️  amend meta tags
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- interface/src/components/layout/Head.tsx
+++ interface/src/components/layout/Head.tsx
@@ -15,17 +15,38 @@ export const Head = (props: Props) => {
         content={props.description ?? SITE_DESCRIPTION}
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:creator" content="@pcaversaccio" />
+
       <meta property="og:url" content="https://www.createx.rocks" />
       <meta property="og:title" content="CreateX" />
       <meta
         property="og:description"
         content="A Trustless, Universal Contract Deployer"
       />
+      <meta property="og:image:type" content="image/png" />
       <meta
         property="og:image"
         content="https://github-production-user-asset-6210df.s3.amazonaws.com/25297591/272914952-38a5989c-0113-427d-9158-47646971b7d8.png"
+      />
+      <meta
+        property="og:image:secure_url"
+        content="https://github-production-user-asset-6210df.s3.amazonaws.com/25297591/272914952-38a5989c-0113-427d-9158-47646971b7d8.png"
+      />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="514" />
+      <meta
+        property="og:image:alt"
+        content="CreateX – A Trustless, Universal Contract Deployer"
+      />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content="@pcaversaccio" />
+      <meta
+        property="twitter:image"
+        content="https://github-production-user-asset-6210df.s3.amazonaws.com/25297591/272914952-38a5989c-0113-427d-9158-47646971b7d8.png"
+      />
+      <meta
+        property="twitter:image:alt"
+        content="CreateX – A Trustless, Universal Contract Deployer"
       />
     </NextHead>
   );
