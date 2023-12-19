0ba3c84e56c80ce40a0617ec70ff3ca9b018d96e
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-12-19 20:44:15 +0100

    🔥 Add Test Network Deployments
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -489,6 +489,9 @@ const config: HardhatUserConfig = {
       moonbeam: vars.get("MOONBEAM_API_KEY", ""),
       moonriver: vars.get("MOONBEAM_API_KEY", ""),
       moonbaseAlpha: vars.get("MOONBEAM_API_KEY", ""),
+      // For Celo testnet & mainnet
+      alfajores: vars.get("CELO_API_KEY", ""),
+      celo: vars.get("CELO_API_KEY", ""),
       // For Harmony testnet & mainnet
       harmony: vars.get("HARMONY_API_KEY", ""),
       harmonyTest: vars.get("HARMONY_API_KEY", ""),
@@ -521,6 +524,9 @@ const config: HardhatUserConfig = {
       // For Mantle testnet & mainnet
       mantle: vars.get("MANTLE_API_KEY", ""),
       mantleTestnet: vars.get("MANTLE_API_KEY", ""),
+      // For Filecoin testnet & mainnet
+      filecoin: vars.get("FILECOIN_API_KEY", ""),
+      filecoinTestnet: vars.get("FILECOIN_API_KEY", ""),
       // For Scroll testnet & mainnet
       scroll: vars.get("SCROLL_API_KEY", ""),
       scrollTestnet: vars.get("SCROLL_API_KEY", ""),
@@ -554,8 +560,8 @@ const config: HardhatUserConfig = {
         network: "optimisticSepolia",
         chainId: 11155420,
         urls: {
-          apiURL: "https://optimism-sepolia.blockscout.com/api",
-          browserURL: "https://optimism-sepolia.blockscout.com",
+          apiURL: "https://api-sepolia-optimistic.etherscan.io/api",
+          browserURL: "https://sepolia-optimism.etherscan.io",
         },
       },
       {
@@ -564,6 +570,22 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: "https://gnosis-chiado.blockscout.com/api",
           browserURL: "https://gnosis-chiado.blockscout.com",
+        },
+      },
+      {
+        network: "celo",
+        chainId: 42220,
+        urls: {
+          apiURL: "https://explorer.celo.org/mainnet/api",
+          browserURL: "https://explorer.celo.org/mainnet",
+        },
+      },
+      {
+        network: "alfajores",
+        chainId: 44787,
+        urls: {
+          apiURL: "https://explorer.celo.org/alfajores/api",
+          browserURL: "https://explorer.celo.org/alfajores",
         },
       },
       {
@@ -626,7 +648,8 @@ const config: HardhatUserConfig = {
         network: "bobaTestnet",
         chainId: 2888,
         urls: {
-          apiURL: "https://api-testnet.bobascan.com/api",
+          apiURL:
+            "https://api.routescan.io/v2/network/testnet/evm/2888/etherscan",
           browserURL: "https://testnet.bobascan.com",
         },
       },
@@ -700,6 +723,22 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: "https://explorer.testnet.mantle.xyz/api",
           browserURL: "https://explorer.testnet.mantle.xyz",
+        },
+      },
+      {
+        network: "filecoin",
+        chainId: 314,
+        urls: {
+          apiURL: "https://filfox.info/api/v1/tools/verifyContract",
+          browserURL: "https://filfox.info/en",
+        },
+      },
+      {
+        network: "filecoinTestnet",
+        chainId: 314159,
+        urls: {
+          apiURL: "https://calibration.filfox.info/api/v1/tools/verifyContract",
+          browserURL: "https://calibration.filfox.info/en",
         },
       },
       {
