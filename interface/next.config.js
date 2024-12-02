26f75cdc2b2b025e71e23104d4c28398f452bfe3
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-12-02 17:49:13 +0100

    ♻️  Refactor `interface` `pnpm` Scripts
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- interface/next.config.js
+++ interface/next.config.js
@@ -3,30 +3,34 @@
 /**
  * @type {import('next').NextConfig}
  **/
-const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-   */
-  output: "export",
+const nextConfig =
+  // Check if running in a Continuous Integration (CI) environment.
+  process.env.CI === "true"
+    ? {
+        /**
+         * Enable static exports for the App Router.
+         * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports.
+         */
+        output: "export",
 
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
+        /**
+         * Disable server-based image optimization. Next.js does not support
+         * dynamic features with static exports.
+         * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized.
+         */
+        images: {
+          unoptimized: true,
+        },
 
-  /**
-   * Use a custom build directory instead of `.next`.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/next-config-js/distDir
-   */
-  distDir: "dist",
-};
+        /**
+         * Use a custom build directory instead of `.next`.
+         * @see https://nextjs.org/docs/pages/api-reference/next-config-js/distDir.
+         */
+        distDir: "dist",
+      }
+    : // For local development, just set a custom build directory instead of `.next`.
+      {
+        distDir: "dist",
+      };
 
 module.exports = nextConfig;
